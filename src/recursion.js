d363a28be80d90bb5af5e5224bb9f3bc00fa0d82
Deb Johnson <friendswithdrj@gmail.com>
2020-09-08 16:00:26 -0500

    Add logic to pass ifEven fn tests

--- src/recursion.js
+++ src/recursion.js
@@ -52,6 +52,20 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n < 0) {
+    n = Math.abs(n);
+  }
+  if (n === 0) {
+    return true;
+  }
+  if (n === 1) {
+    return false;
+  } else {
+    n = n -2;
+    return isEven(n);
+  }
+
+
 };
 
 // 5. Sum all integers below a given integer.
