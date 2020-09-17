5c6cc7ef44558b8213c5d7913c1e7971d8ffaa72
Deb Johnson <friendswithdrj@gmail.com>
2020-09-17 15:42:18 -0500

    Edit modulo fn to pass tests

--- src/recursion.js
+++ src/recursion.js
@@ -189,10 +189,10 @@ var modulo = function(x, y) {
   if (x === 0) {
     return 0;
   }
-  if (x < 0 && y > 0) {
+  if (x < 0) {
     return -modulo(-x, y);
   }
-  if (x > 0 && y < 0) {
+  if (y < 0) {
     return modulo(x, -y);
   }
   if (x < y) {
