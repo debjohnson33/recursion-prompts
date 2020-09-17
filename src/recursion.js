ff5f28ae21d88ecbcd47b12e6a72284aae2c7656
Deb Johnson <friendswithdrj@gmail.com>
2020-09-17 16:48:08 -0500

    Save progress on multiply fn

--- src/recursion.js
+++ src/recursion.js
@@ -204,6 +204,29 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (x === 0 || y === 0) {
+    return 0;
+  }
+  if (x === 1) {
+    return y;
+  }
+  if (y === 1) {
+    return x;
+  }
+  if (x < 0 && y < 0) {
+    return x + multiply(-x, -y);
+  }
+  if (x < 0) {
+    return multiply(-x, y - 1);
+  }
+  if (y > 0) {
+    return x + multiply(x, y - 1);
+  }
+  if (y < 0) {
+    return -multiply(x, -y);
+  }
+
+  return x + multiply(x, y - 1);
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
